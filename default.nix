13e11e8f12909bc16b4c7ec4076dc1c9910f87cb
Andreas Baldeau <andreas@baldeau.net>
2017-09-08 13:20:51 +0200

    Fixing phantomjs for unit testing on NixOS.

--- default.nix
+++ default.nix
@@ -7,6 +7,28 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     nodejs
+    phantomjs
   ];
+
+  shellHook = ''
+    echo
+    echo "========================"
+    echo "Setting up node modules."
+    echo "========================"
+    echo
+
+    npm install
+
+    echo
+    echo "================="
+    echo "Fixing phantomjs."
+    echo "================="
+    echo
+
+    rm -f node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs
+    ln -s ${phantomjs}/bin/phantomjs node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs
+
+    echo
+  '';
 }
 
