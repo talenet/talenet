a6d56f4c171afbd06f5d7fa742fce2973f54eec0
Andreas Baldeau <andreas@baldeau.net>
2017-09-08 13:20:27 +0200

    Scaffolded first vue.js setup.
    E2E / Selenium tests are currently broken. We need to decide if
    we want to maintain those or otherwise remove them.

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to TALEnet, the intergalactic skill & idea sharing community.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
