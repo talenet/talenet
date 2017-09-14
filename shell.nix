ee1818542e52b52caa92b62d84e8b79a35a505a1
Andreas Baldeau <andreas@baldeau.net>
2017-09-14 17:21:35 +0200

    Replaced default.nix with shell.nix that supports electron.

---
+++ shell.nix
@@ -0,0 +1,33 @@
+with import <nixpkgs> {};
+with pkgs;
+
+let taleEnv = buildEnv {
+  name = "TALEenv";
+  paths = [
+    stdenv.cc.cc.lib zlib glib dbus.lib gnome2.gtk atk pango.out freetype libgnome_keyring3
+    fontconfig.lib gdk_pixbuf cairo cups expat libgpgerror alsaLib nspr gnome3.gconf nss
+    xorg.libXrender xorg.libX11 xorg.libXext xorg.libXdamage xorg.libXtst
+    xorg.libXcomposite xorg.libXi xorg.libXfixes xorg.libXrandr
+    xorg.libXcursor libcap systemd libnotify udev udev.lib bzip2 readline xorg.libxcb
+    xorg.libXScrnSaver fuse
+  ];
+}; in
+
+(pkgs.buildFHSUserEnv {
+  name = "TALEnet";
+
+  targetPkgs = pkgs: (with pkgs; [
+    nodejs
+    taleEnv
+  ]);
+
+  extraBuildCommands = ''
+    (cd usr/lib64 && ln -sv libbz2.so.1.0.* libbz2.so.1.0)
+  '';
+
+  profile = ''
+    export npm_config_cache="/tmp/talenet-npm-cache/"
+    export ELECTRON_CACHE="/tmp/talenet-electron-cache/"
+  '';
+}).env
+
