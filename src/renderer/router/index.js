2ac0c28eed1390e66a073f07c9b03d4ef2e8ada0
Andreas Baldeau <andreas@baldeau.net>
2017-09-27 14:18:37 +0200

    Some finding from debugging on URL hash issue documented.

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,6 +6,7 @@ import CatCodes from '@/components/CatCodes.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'hash', // As Electron uses file:// in production we cannot use 'history' there.
   routes: [
     {
       path: '/',
