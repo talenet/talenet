8021ada456147dd6a280a151afa04ca316acad91
Henry <cryptix@riseup.net>
2017-09-30 19:19:17 +0200

    ssb-client as a vuex module
    Change-Id: I1ca9e621b4cff0427042a28bc6474b9661bbe15f

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import i18n from '../i18n'
 import page from './page'
+import ssb from './ssb'
 
 // Make vue.js use the vuex.js plugin. This also enables the possibility to inject the
 // store into the components.
@@ -12,18 +13,7 @@ export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production', // prevent state changes outside of mutations
 
   modules: {
+    ssb: ssb,
     page: page({ i18n, document })
-  },
-
-  state: {
-    loggedIn: false // simple example during setup phase ;-)
-  },
-  getters: {
-    loggedOut: state => !state.loggedIn
-  },
-  mutations: {
-    login: state => {
-      state.loggedIn = true
-    }
   }
 })
