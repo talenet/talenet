f7ab4f85dd47f387e05fff849de068b3fa48ddc5
Henry <cryptix@riseup.net>
2017-10-02 03:20:45 +0200

    home: remove login button tests
    Change-Id: I02ee64c43a344f969f8b103423271982ddf3d309

--- test/unit/specs/components/Home.spec.js
+++ test/unit/specs/components/Home.spec.js
@@ -9,39 +9,12 @@ describe('Home.vue', () => {
   let store
 
   beforeEach(() => {
-    // Mocked store for testing. As soon as the real store triggers actions in ssb this becomes more relevant.
-    store = new Vuex.Store({
-      state: {
-        loggedIn: false
-      },
-      getters: {
-        loggedOut: state => !state.loggedIn
-      },
-      mutations: {
-        login: state => {
-          state.loggedIn = true
-        }
-      }
-    })
+    store = new Vuex.Store({ })
   })
 
   it('should render correct contents', () => {
     const home = mount(Home, { store })
     expect(home.find('.home h1')[0].text())
       .to.equal('Welcome to TALEnet, the intergalactic skill & idea sharing community.')
-  })
-
-  it('should initially show login button', () => {
-    const home = mount(Home, { store })
-    expect(home.find('button.login')[0].text())
-      .to.equal('Login')
-  })
-
-  it('should hide login button on click', () => {
-    const home = mount(Home, { store })
-    const button = home.find('button.login')[0]
-    button.trigger('click')
-    expect(home.find('button.login').length)
-      .to.equal(0)
   })
 })
