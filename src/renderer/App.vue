28050d5e5059703606e8bc05970ff277749adabd
Henry <cryptix@riseup.net>
2017-10-04 01:06:29 +0200

    show errors in a modal
    Change-Id: I1891bbe05d5867d51b56005fbcaec76ebcf7a067

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -14,14 +14,30 @@
         <b-nav is-nav-bar class="ml-auto">
           <!-- TODO: ssb heartbeat? -->
           <b-nav v-if="ssbConnected">
-           <b-nav-item disabled>Connected as {{ ssbShort }}</b-nav-item>
+            <b-nav-item disabled>Connected as {{ ssbShort }}</b-nav-item>
           </b-nav>
           <b-nav v-if="!ssbConnected">
-           <b-button class="mx-2 my-2 my-sm-0" @click="reconnect" variant="success">Reconnect</b-button>
+            <b-button class="mx-2 my-2 my-sm-0" @click="reconnect" variant="success">Reconnect</b-button>
           </b-nav>
         </b-nav>
       </b-collapse>
     </b-navbar>
+
+    <!-- error modal -->
+    <b-container>
+      <b-modal v-model="showError"
+        title="Error Occured"
+        header-bg-variant="danger"
+        >
+
+        <h4>{{ getError.message }}</h4>
+        <pre>{{ getError.stack }}</pre>
+        <footer slot="modal-footer" class="w-100">
+          <small class="float-left">TODO: report error button</small>
+          <b-btn size="sm" class="float-right" variant="default" @click="acceptError">OK</b-btn>
+        </footer>
+      </b-modal>
+    </b-container>
 
     <b-container>
       <router-view></router-view>
@@ -30,51 +46,71 @@
 </template>
 
 <script>
-  import { updateTitleFromRoute } from './util/page'
-  import {mapGetters} from 'vuex'
+import { updateTitleFromRoute } from './util/page'
+import { mapGetters } from 'vuex'
 
-  /**
-   * This componet holds the whole app.
-   */
-  export default {
-    name: 'app',
+/**
+ * This componet holds the whole app.
+ */
+export default {
+  name: 'app',
 
-    created () {
-      updateTitleFromRoute(this.$route, this.$store)
+  created () {
+    updateTitleFromRoute(this.$route, this.$store)
+    this.$store.dispatch('ssb/connect')
+  },
+
+  computed: {
+    ...mapGetters({
+      ssbConnected: 'ssb/connected'
+      // ssbShort: 'ssb/whoami'
+    }),
+    ssbShort () {
+      return this.$store.state.ssb.id.slice(0, 10) + '...'
+    },
+
+    showError: {
+      get () {
+        return this.$store.state.err != null
+      },
+
+      set () {} // ignored
+    },
+
+    getError () {
+      if (this.$store.state.err == null) {
+        return new Error('should not show up')
+      }
+      // console.error(this.$store.state.err)
+      return this.$store.state.err
+    }
+  },
+
+  methods: {
+    acceptError () {
+      this.$store.commit('error', null)
+    },
+
+    reconnect () {
       this.$store.dispatch('ssb/connect')
-    },
+    }
+  },
 
-    computed: {
-      ...mapGetters({
-        ssbConnected: 'ssb/connected'
-        // ssbShort: 'ssb/whoami'
-      }),
-      ssbShort () {
-        return this.$store.state.ssb.id.slice(0, 10) + '...'
-      }
-    },
-
-    methods: {
-      reconnect () {
-        this.$store.dispatch('ssb/connect')
-      }
-    },
-
-    watch: {
-      '$route' (route) {
-        updateTitleFromRoute(route, this.$store)
-      }
+  watch: {
+    '$route' (route) {
+      updateTitleFromRoute(route, this.$store)
     }
   }
+}
 </script>
 
 <style lang="scss">
-  // App SCSS intialization block.
-  @import "variables";
-  @import "../../node_modules/bootstrap/scss/bootstrap";
-  @import "global-fixes";
+// App SCSS intialization block.
+@import "variables";
+@import "../../node_modules/bootstrap/scss/bootstrap";
+@import "global-fixes";
 </style>
 
 <style scoped lang="scss">
-  // Component specific styling.
+// Component specific styling.
 </style>
