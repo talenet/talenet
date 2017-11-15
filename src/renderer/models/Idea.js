403305e47f5dfbe99c412fa848fa23d6e905317c
Andreas Baldeau <andreas@baldeau.net>
2017-11-15 17:56:54 +0100

    Merge idea updates only for outdated fields
    Change-Id: I3f59caf07862dbcdb0da9884756d08f0a9bfddc0

--- src/renderer/models/Idea.js
+++ src/renderer/models/Idea.js
@@ -8,8 +8,10 @@ const FIELDS = ['key', 'title', 'description']
  * Immutable class holding data for an idea.
  */
 export default class Idea {
-  constructor (data) {
+  constructor (data, optTimestamps) {
     this._data = filterFields(data || {}, FIELDS)
+    this._timestamps = optTimestamps || {}
+
     addGetters(this, this._data, FIELDS)
   }
 
@@ -20,11 +22,29 @@ export default class Idea {
     })
   }
 
+  /**
+   * Merge the idea with an update from SSB. This tracks for each field a timestamp
+   * so that each field will only be overwritten by an update for a field with a newer timestamp.
+   */
   withSsbUpdate (msg) {
-    return new Idea({
-      ...this._data,
-      ...msg.value.content
-    })
+    const timestamp = msg.value.timestamp
+    const content = msg.value.content
+
+    const newData = {}
+    const newTimestamps = {}
+
+    for (const field of FIELDS) {
+      if (content.hasOwnProperty(field) &&
+        (!this._timestamps.hasOwnProperty(field) || this._timestamps[field] < timestamp)) {
+        newData[field] = content[field]
+        newTimestamps[field] = timestamp
+      } else {
+        newData[field] = this._data[field]
+        newTimestamps[field] = this._timestamps[field]
+      }
+    }
+
+    return new Idea(newData, newTimestamps)
   }
 
   asUpdate () {
