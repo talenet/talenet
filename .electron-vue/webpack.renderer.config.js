f89163b3ba2b26b50ffe0fdb1c0051d270491f80
Andreas Baldeau <andreas@baldeau.net>
2017-12-18 22:42:57 +0100

    Fixed warning: chloride uses an expression as dependency.
    * Had to fixate the dependency for renderer.
    
    Change-Id: I493a180baeb4923b6cb330e86ec6e829c6decc4c

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -141,7 +141,8 @@ let rendererConfig = {
   resolve: {
     alias: {
       '@': path.join(__dirname, '../src/renderer'),
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      'chloride': 'chloride/bindings'
     },
     extensions: ['.js', '.vue', '.json', '.css', '.node']
   },
