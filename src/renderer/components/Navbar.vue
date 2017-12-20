0339bf1777298b4844e0b793b36181e9c69ff5d9
Andreas Baldeau <andreas@baldeau.net>
2017-12-20 23:43:30 +0100

    Burger menu optimizations.
    Change-Id: Ic8ed626a9e69dab1cdd4309153f5b11fc1c34a7e

--- src/renderer/components/Navbar.vue
+++ src/renderer/components/Navbar.vue
@@ -1,15 +1,14 @@
 <template>
   <b-navbar :fixed="position" :class="classes" toggleable="sm" type="dark" variant="dark">
-    <b-nav-toggle target="t-navbar-top-collapse"></b-nav-toggle>
-
-    <b-collapse is-nav id="t-navbar-top-collapse">
+    <b-nav-toggle :target="'t-navbar-' + position + '-collapse'"></b-nav-toggle>
+    <b-collapse is-nav :id="'t-navbar-' + position + '-collapse'">
       <b-navbar-nav class="container d-flex d-flex-row">
         <slot name="item"></slot>
-        <div class="t-navbar-center">
-          <slot name="center"></slot>
-        </div>
       </b-navbar-nav>
     </b-collapse>
+    <div class="t-navbar-center">
+      <slot name="center"></slot>
+    </div>
   </b-navbar>
 </template>
 
@@ -33,16 +32,21 @@
 <style lang="scss">
   // Not scoped to allow styling slots.
   @import "../variables";
+  @import "../mixins";
 
   .t-navbar {
     height: $navbar-content-height;
 
-    &.t-navbar-top {
+    &.t-navbar-top, &.t-navbar-top .navbar-nav {
       border-bottom: $navbar-border-width solid $navbar-border-color;
     }
 
     &.t-navbar-bottom {
       border-top: $navbar-border-width solid $navbar-border-color;
+    }
+
+    .navbar-toggler {
+      outline: none;
     }
 
     .navbar-nav {
@@ -52,8 +56,7 @@
 
     .nav-item, .form-inline {
       flex-grow: 1;
-      border-left: $navbar-border-width solid $navbar-border-color;
-      max-width: 25%;
+      border: none;
       height: $navbar-content-height;
       max-height: $navbar-content-height;
       padding: 0;
@@ -65,6 +68,33 @@
         border: none;
         height: $navbar-content-height;
         line-height: $navbar-content-height;
+      }
+    }
+
+    .navbar-nav {
+      border: $navbar-border-width solid $navbar-border-color;
+      background-color: $navbar-bg;
+    }
+
+    &.t-navbar-top .navbar-nav {
+      padding-top: $navbar-center-size * 0.4;
+    }
+
+    &.t-navbar-bottom .navbar-nav {
+      padding-bottom: $navbar-center-size * 0.4;
+    }
+
+    @include media-breakpoint-up(sm) {
+      .navbar-nav {
+        padding-top: 0;
+        padding-bottom: 0;
+      }
+
+      .nav-item, .form-inline {
+        max-width: 25%;
+        border-left: $navbar-border-width solid $navbar-border-color;
+        border-top: none;
+        border-bottom: none;
       }
     }
 
