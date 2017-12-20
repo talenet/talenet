1cf087eb42a90db82a3372ecb44d936793fbfb94
Andreas Baldeau <andreas@baldeau.net>
2017-12-20 15:11:18 +0100

    Introduction box for my:config.
    Change-Id: I73191aede86b906bbff45d22f65e2007717156ff

--- src/renderer/components/SettingsPage.vue
+++ src/renderer/components/SettingsPage.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="row t-settings">
-    <div class="t-center-col">
-      <t-invite-accept-form
-        :join-pub-button-text="$t('settings.invite.form.joinPub.button')"
-        :cancel-button-text="$t('settings.invite.form.cancel.button')"></t-invite-accept-form>
+  <div class="t-settings">
+    <div class="row">
+      <div class="t-center-col">
+        <t-introduction-box messagesKey="settings.introduction"></t-introduction-box>
+      </div>
+    </div>
 
-      <t-action-panel :text="$t('settings.introductions.text')">
-        <b-button
-          slot="left"
-          variant="primary"
-          @click="resetIntroductions()">
-          {{$t('settings.introductions.reset.button')}}
-        </b-button>
-      </t-action-panel>
-      <t-action-panel :text="$t('settings.devMode.text')">
-        <b-button
-          v-if="!isDevMode"
-          slot="left"
-          variant="outline-danger"
-          @click="setDevMode(true)">
-          {{$t('settings.devMode.enable.button')}}
-        </b-button>
-        <b-button
-          v-if="isDevMode"
-          slot="left"
-          variant="outline-danger"
-          @click="setDevMode(false)">
-          {{$t('settings.devMode.disable.button')}}
-        </b-button>
-      </t-action-panel>
+    <div class="row">
+      <div class="t-center-col">
+        <t-invite-accept-form
+          :join-pub-button-text="$t('settings.invite.form.joinPub.button')"
+          :cancel-button-text="$t('settings.invite.form.cancel.button')"></t-invite-accept-form>
+
+        <t-action-panel :text="$t('settings.introductions.text')">
+          <b-button
+            slot="left"
+            variant="primary"
+            @click="resetIntroductions()">
+            {{$t('settings.introductions.reset.button')}}
+          </b-button>
+        </t-action-panel>
+        <t-action-panel :text="$t('settings.devMode.text')">
+          <b-button
+            v-if="!isDevMode"
+            slot="left"
+            variant="outline-danger"
+            @click="setDevMode(true)">
+            {{$t('settings.devMode.enable.button')}}
+          </b-button>
+          <b-button
+            v-if="isDevMode"
+            slot="left"
+            variant="outline-danger"
+            @click="setDevMode(false)">
+            {{$t('settings.devMode.disable.button')}}
+          </b-button>
+        </t-action-panel>
+      </div>
     </div>
   </div>
 </template>
