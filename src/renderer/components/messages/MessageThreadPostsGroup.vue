968f248ec140176ad4d855ab356a0d6a71027141
Andreas Baldeau <andreas@baldeau.net>
2018-02-19 18:15:04 +0100

    my:messages - cut corners for thread view
    Change-Id: Ibf17b46e02f097c02a383b260c0495784ffce084

--- src/renderer/components/messages/MessageThreadPostsGroup.vue
+++ src/renderer/components/messages/MessageThreadPostsGroup.vue
@@ -1,9 +1,21 @@
 <template>
   <div :class="classes">
-    <div class="t-message-thread-posts-group-message" v-for="msg in messages" :key="msg.key">
-      <span>{{msg.value.timestamp | tFormatTimestamp}}</span>
-      <t-markdown-text :text="msg.value.content.text"></t-markdown-text>
+    <div>
+      <div class="t-message-thread-posts-group-message" v-for="msg in messages" :key="msg.key">
+        <span class="t-message-thread-posts-group-message-timestamp">{{msg.value.timestamp | tFormatTimestamp}}</span>
+        <t-markdown-text :text="msg.value.content.text"></t-markdown-text>
+      </div>
     </div>
+
+    <svg v-if="isOwn" class="t-message-thread-posts-group-corner-left">
+      <polygon class="t-message-thread-posts-group-corner-bg" points="0,0 13,24 13,0"></polygon>
+      <path class="t-message-thread-posts-group-corner-border" d="M0,0 L13,24"></path>
+    </svg>
+
+    <svg v-if="!isOwn" class="t-message-thread-posts-group-corner-right">
+      <polygon class="t-message-thread-posts-group-corner-bg" points="13,0 0,24 0,0"></polygon>
+      <path class="t-message-thread-posts-group-corner-border" d="M13,0 L0,24"></path>
+    </svg>
   </div>
 </template>
 
@@ -21,7 +33,7 @@
 
     mixins: [
       SubscriptionMixin({
-        '!': [ 'identity/subscribeOwnIdentityKey' ]
+        '!': ['identity/subscribeOwnIdentityKey']
       })
     ],
 
@@ -50,14 +62,59 @@
   @import "../../variables";
 
   .t-message-thread-posts-group {
-    background-color: $tale-dark-grey;
+    background-color: $message-thread-group-bg;
+    border: $message-thread-group-border-width solid $message-thread-group-border-color;
 
-    margin: {
-      bottom: 1.5rem;
-    }
+    margin-bottom: $message-thread-group-spacing;
+    padding: 0;
 
     .t-message-thread-posts-group-message {
-      border-bottom: 1px solid $tale-black;
+      position: relative;
+
+      border-bottom: $message-thread-group-border-width solid $message-thread-group-border-color;
+      padding: {
+        top: $message-thread-group-padding-y;
+        left: $message-thread-group-padding-x;
+        right: $message-thread-group-padding-x;
+        bottom: $message-thread-group-padding-bottom;
+      }
+
+      &:last-child {
+        border: none;
+      }
+    }
+
+    .t-message-thread-posts-group-corner-left,
+    .t-message-thread-posts-group-corner-right {
+      position: absolute;
+      bottom: -$message-thread-group-border-width;
+
+      width: 13px;
+      height: 24px;
+      background-color: $body-bg;
+
+      .t-message-thread-posts-group-corner-bg {
+        fill: $message-thread-group-bg;
+      }
+
+      .t-message-thread-posts-group-corner-border {
+        stroke: $message-thread-group-border-color;
+        stroke-width: $message-thread-group-border-width;
+      }
+    }
+
+    .t-message-thread-posts-group-corner-left {
+      left: -$message-thread-group-border-width;
+    }
+
+    .t-message-thread-posts-group-corner-right {
+      right: -$message-thread-group-border-width;
+    }
+
+    .t-message-thread-posts-group-message-timestamp {
+      position: absolute;
+      bottom: $message-thread-group-padding-y;
+      right: $message-thread-group-padding-x;
     }
   }
 </style>
